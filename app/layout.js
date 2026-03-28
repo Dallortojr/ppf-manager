ec21922a05ad70fd3debbce1d39e8d1f04d5ff5a
Dallortojr <dallortojr@gmail.com>
2026-03-28 10:56:55 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+title: "PPF Manager",
+description: "PPF scheduling app",
+};
+
+export default function RootLayout({ children }) {
+return (
+<html lang="en">
+<body>{children}</body>
+</html>
+);
+}
